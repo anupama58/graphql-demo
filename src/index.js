bf902d016a6502c01e2e1d9280e7aa3a45dccbf0
anupama <anupama.pawar@sts.in>
2021-09-27 23:37:21 +0530

    addition of wrapping

--- src/index.js
+++ src/index.js
@@ -20,35 +20,60 @@ const resolvers = {
     },
     Mutation:{
         signup: (parent, args) => {
-            return fetch(`${baseURL}/user/sign-up`)
+            const configJson = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify(args),
+            }
+
+            return fetch(`${baseURL}/user/sign-up`,configJson)
             .then(res => res.json())
-            .then((data) => {
-                console.log(data)
+            .then((user) => {
+                console.log(user)
                 return {
-                    message:'hello',
-                    token:'ksjjka'
+                    message:user.user.message,
+                    token:user.user.token
                 }
             })
         },
-        login: (parent, args) => {
-            return fetch(`${baseURL}/user/login`)
+        login: async (parent, args) => {
+            const configJson = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify(args),
+            }
+           return fetch(`${baseURL}/user/login`,configJson)
             .then(res => res.json())
-            .then((data) => {
-                console.log(data)
+            .then((user) => {
+                console.log(user.user.token)
                 return {
-                    message:'hello',
-                    token:'ksjjka'
+                    message:user.user.token,
+                    token:user.user.token
+                }
+           })
+        },
+        update: (parent, args) => {
+            const configJson = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify(args),
+            }
+            return fetch(`${baseURL}/user/update`)
+            .then(res => res.json())
+            .then((updateData) => {
+                console.log(updateData)
+                return{
+                    message:updateData.user.message,
+                    result:updateData.user.result
                 }
             })
         },
-        // update: (parent, args) => {
-        //     return fetch(`${baseURL}/user/`)
-        //     .then(res => res.json())
-        //     .then((data) => {
-        //         console.log(data)
-                
-        //     })
-        // },
 
     }
 }
